dd3208b0b2eb0d051ab148e6d985a2c3f40a5668
Brian H. Ward <glyphrider@gmail.com>
2025-03-31 14:27:40 -0400

    dri cleanup

--- setup-dri.sh
+++ setup-dri.sh
@@ -4,11 +4,12 @@ set -x
 
 HOST_VIDEO=$(grep video /etc/group | cut -d: -f3)
 HOST_RENDER=$(grep render /etc/group | cut -d: -f3)
-incus exec jellyfin groupadd -g $HOST_VIDEO host_video
-incus exec jellyfin groupadd -g $HOST_RENDER host_render
-incus exec jellyfin usermod -aG host_video jellyfin
-incus exec jellyfin usermod -aG host_render jellyfin
+incus exec jellyfin -- groupadd -g $HOST_VIDEO host_video
+incus exec jellyfin -- groupadd -g $HOST_RENDER host_render
+incus exec jellyfin -- usermod -aG host_video jellyfin
+incus exec jellyfin -- usermod -aG host_render jellyfin
 incus stop jellyfin
+incus profile add jellyfin bridged
 incus config device override jellyfin eth0 hwaddr='00:16:3E:91:31:07'
 incus config device add jellyfin dri disk source=/dev/dri path=/dev/dri
 incus start jellyfin
